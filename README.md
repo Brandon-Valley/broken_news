cc2c436623db7aa692cd77e0b8c31386bd4c862f
Brandon <bavvh8@mst.edu>
2022-03-05 14:32:43 -0600

    saves but cant play - sounds awful

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # broken_news
+
+## Setup
+
+---
+
+1. Generate API key and secret: https://uberduck.ai/account/manage
+2. Set those as env. vars:
+    * UBERDUCK_Key
+    * UBERDUCK_Secret
---
+++ src/uberduck_utils.py
@@ -0,0 +1,9 @@
+import uberduckapi as ud
+import os
+my_duck = ud.UberDuck(os.environ['UBERDUCK_Key'], os.environ['UBERDUCK_Secret'])
+sponge = my_duck.get_voice('rick-sanchez', "Hey everyone I'm alive")
+
+# if the request went through
+if sponge:
+    # sponge.play_voice()
+    sponge.save('spongebob.wav')
