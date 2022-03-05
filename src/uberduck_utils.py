a3df629123280a720e364f69054e40e00837c0f0
Brandon <bavvh8@mst.edu>
2022-03-05 16:34:51 -0600

    sounds ok-ish

--- src/uberduck_utils.py
+++ src/uberduck_utils.py
@@ -1,12 +1,14 @@
 import uberduckapi as ud
 import os
 my_duck = ud.UberDuck(os.environ['UBERDUCK_Key'], os.environ['UBERDUCK_Secret'])
-# sponge = my_duck.get_voice('rick-sanchez', "Hey everyone I'm alive")
-sponge = my_duck.get_voice('fish-head', "Breaking news! Hey everyone I'm alive")
+# sponge = my_duck.get_voice('fish-head', "Breaking news! Hey everyone I'm alive")
+# sponge = my_duck.get_voice('fish-head', "test test test ... ... ... Breaking news! Russian customs officials said they had detained a star American basketball player after finding hashish oil in her luggage at an airport near Moscow. The Russian news agency has identified the player as Brittney Griner.")
+sponge = my_duck.get_voice('fish-head', "... Breaking news! Russian customs officials said they had detained a star American basketball player after finding hashish oil in her luggage at an airport near Moscow. ... ... The Russian news agency has identified the player as Brittney Griner.")
 
 # if the request went through
 if sponge:
     # sponge.play_voice()
-    sponge.save('fish.wav')
+    sponge.save('fish.mp4') # also works with .wav
     
     
+print('done')
