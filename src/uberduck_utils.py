86566798c0e89b4f3cddec9871791d3cab372fac
Brandon <bavvh8@mst.edu>
2022-03-05 19:59:24 -0600

    works

--- src/uberduck_utils.py
+++ src/uberduck_utils.py
@@ -1,14 +1,35 @@
 import uberduckapi as ud
 import os
-my_duck = ud.UberDuck(os.environ['UBERDUCK_Key'], os.environ['UBERDUCK_Secret'])
-# sponge = my_duck.get_voice('fish-head', "Breaking news! Hey everyone I'm alive")
-# sponge = my_duck.get_voice('fish-head', "test test test ... ... ... Breaking news! Russian customs officials said they had detained a star American basketball player after finding hashish oil in her luggage at an airport near Moscow. The Russian news agency has identified the player as Brittney Griner.")
-sponge = my_duck.get_voice('fish-head', "... Breaking news! Russian customs officials said they had detained a star American basketball player after finding hashish oil in her luggage at an airport near Moscow. ... ... The Russian news agency has identified the player as Brittney Griner.")
+# my_duck = ud.UberDuck(os.environ['UBERDUCK_Key'], os.environ['UBERDUCK_Secret'])
+# # sponge = my_duck.get_voice('fish-head', "Breaking news! Hey everyone I'm alive")
+# # sponge = my_duck.get_voice('fish-head', "test test test ... ... ... Breaking news! Russian customs officials said they had detained a star American basketball player after finding hashish oil in her luggage at an airport near Moscow. The Russian news agency has identified the player as Brittney Griner.")
+# sponge = my_duck.get_voice('fish-head', "... Breaking news! Russian customs officials said they had detained a star American basketball player after finding hashish oil in her luggage at an airport near Moscow. ... ... The Russian news agency has identified the player as Brittney Griner.")
+#
+# # if the request went through
+# if sponge:
+#     # sponge.play_voice()
+#     sponge.save('fish.mp4') # also works with .wav
+#
+#
+# print('done')
 
-# if the request went through
-if sponge:
-    # sponge.play_voice()
-    sponge.save('fish.mp4') # also works with .wav
+
+
+def write_voice_clip(out_audio_file_path, script_str, voice_name_str = 'fish-head'):
+    my_duck = ud.UberDuck(os.environ['UBERDUCK_Key'], os.environ['UBERDUCK_Secret'])
+    # voice = my_duck.get_voice('fish-head', "... Breaking news! Russian customs officials said they had detained a star American basketball player after finding hashish oil in her luggage at an airport near Moscow. ... ... The Russian news agency has identified the player as Brittney Griner.")
+    voice = my_duck.get_voice(voice_name_str, script_str)
+
+    if voice:
+        voice.save(out_audio_file_path)
+    else:
+        raise Exception("ERROR: Voice not read from Uberduck API Correctly")
     
     
-print('done')
+if __name__ == "__main__":
+    SCRIPT_PARENT_DIR_PATH = os.path.abspath(os.path.dirname(__file__))
+    OUT_AUDIO_FILE_PATH = os.path.join(SCRIPT_PARENT_DIR_PATH, 'fish_head_voice_clip_test.mp4')
+    # script_str = "... Breaking news! Russian customs officials said they had detained a star American basketball player after finding hashish oil in her luggage at an airport near Moscow. ... ... The Russian news agency has identified the player as Brittney Griner."
+    script_str = "Breaking news! Russian customs officials "
+    write_voice_clip(OUT_AUDIO_FILE_PATH, script_str, 'fish-head')
+    print('done')
