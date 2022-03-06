45403e7257fe8586d40472dab72918ca3c008637
Brandon <bavvh8@mst.edu>
2022-03-05 18:39:17 -0600

    get_mouth_shape_frame_dl_from_dat_file() works

--- src/animation_utils.py
+++ src/animation_utils.py
@@ -2,6 +2,9 @@ import cv2
 import os
 import subprocess
 
+# from sms.txt_logger import txt_logger
+from sms.logger import txt_logger
+
 SCRIPT_PARENT_DIR_PATH = os.path.abspath(os.path.dirname(__file__))
 IMGS_DIR_PATH = os.path.join(SCRIPT_PARENT_DIR_PATH, '..', 'imgs')
 MOUTH_SHAPE_DAT_FILE_PATH = os.path.join(SCRIPT_PARENT_DIR_PATH, '..', 'imgs')
@@ -41,6 +44,25 @@ def write_mouth_shape_dat_file(in_voice_file_path, out_dat_path, fps):
     cmd = 'rhubarb -o {} {} -f dat --datFrameRate {} --datUsePrestonBlair'.format(out_dat_path, in_voice_file_path, fps)
     subprocess.call(cmd)
     
+def get_mouth_shape_frame_dl_from_dat_file(in_dat_path):
+    print('Creating mouth_shape_frame_dl from ', in_dat_path)
+    line_l = txt_logger.read(in_dat_path)
+    # print(line_l)
+
+    mouth_shape_frame_dl = []
+    
+    # ignore 1st line
+    for line in line_l[1:-1]:
+        frame_num_str, mouth_shape_str = line.split(' ')
+        
+        mouth_shape_frame_dl.append(
+                                        {
+                                            'frame_num'  : int(frame_num_str),
+                                            'mouth_shape': mouth_shape_str.upper()
+                                        }
+            
+                                    )
+    return mouth_shape_frame_dl
 
 def animate_fish_head_test():  
     # write_mouth_shape_dat_file()
@@ -53,6 +75,8 @@ if __name__ == "__main__":
     in_voice_file_path = os.path.join(SCRIPT_PARENT_DIR_PATH, 'fish.wav')
     out_dat_path = os.path.join(SCRIPT_PARENT_DIR_PATH, 'mouth_shapes.dat')
     fps = 24
-    write_mouth_shape_dat_file(in_voice_file_path, out_dat_path, fps)
+    # write_mouth_shape_dat_file(in_voice_file_path, out_dat_path, fps)
+    mouth_shape_frame_dl = get_mouth_shape_frame_dl_from_dat_file(out_dat_path)
+    print(mouth_shape_frame_dl)
 
     print('done')
