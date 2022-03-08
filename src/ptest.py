a0e135de2b793603d37a59ec14cdff3235652398
Brandon <bavvh8@mst.edu>
2022-03-08 17:38:42 -0600

    ptest works

--- src/ptest.py
+++ src/ptest.py
@@ -2,6 +2,6 @@ from simple_image_download import simple_image_download as simp
 
 response = simp.simple_image_download
 
-response().download('bear', 5)
+response().download('bear', 5, image_directory = 'test_dir')
 
 print(response().urls('bear', 5))
